a1f3c2609ada7404a25f58bb6f2f9543a40ba2e0
Rachel <rebgrewcock@gmail.com>
2021-02-14 19:00:46 +0000

    Finish work to refactor palindrome method

--- test/rggrewco_palindrome_test.rb
+++ test/rggrewco_palindrome_test.rb
@@ -19,8 +19,4 @@ class RggrewcoPalindromeTest < Minitest::Test
   def test_palindrome_with_punctuation
     assert "Madam, I'm Adam.".palindrome?
   end
-
-  def test_letters
-    assert_equal "MadamImAdam", "Madam, I'm Adam.".letters
-  end
 end
